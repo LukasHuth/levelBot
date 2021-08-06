a56140e8d1b62f38c4d952f180472f96d7f3f398
Hiroschi - <60547284+LukasHuth@users.noreply.github.com>
2021-08-06 05:29:40 +0200

    Added GET /level/userid; Removed typo in README.md;
    Signed-off-by: Hiroschi - <60547284+LukasHuth@users.noreply.github.com>

--- server.js
+++ server.js
@@ -27,6 +27,49 @@ Status Codes:
 */
 
 // start Server
+
+app.all("/v1/user/:id", (req, res, method) => {
+    console.log(req.method);
+    if(req.method == 'GET') {
+        const { id } = req.params;
+        var data = JSON.parse(fs.readFileSync("./userdata.json"));
+        if(data.hasOwnProperty(id+'')) {
+            data[id+'']["required"] = 100*data[id+'']["level"];
+            data[id+'']["missing"] = data[id+'']['xp'];
+            for(i = 1; i <= data[id+''].level-1;i++) {
+                data[id+'']['missing']-=100*i;
+            }
+            data[id+'']["rank"] = 1;
+            for(elm in data) {
+                if(elm != id+'') {
+                    // console.log(elm, id+'');
+                    if(data[elm]["xp"] > data[id+'']["xp"]) {
+                        data[id+'']["rank"]++;
+                    }
+                }
+            }
+            res.status(200).send(data[id+'']);
+        } else {
+            data[id+''] = {};
+            // "level": 7,
+            // "xp": 2650,
+            // "missing": 150,
+            // "rank": 1,
+            // "required": 400
+            data[id+'']["level"] = 1;
+            data[id+'']["xp"] = 0;
+            fs.writeFileSync("./userdata.json", JSON.stringify(data));
+            data[id+'']["missing"] = 100;
+            data[id+'']["rank"] = -1;
+            data[id+'']["required"] = 100;
+            res.status(200).send(data[id+'']);
+            // res.status(404).send();
+        }
+        return;
+    }
+    res.status(400).send();
+})
+
 app.listen(
     PORT,
     () => console.log(`Started server on port ${PORT}`)
